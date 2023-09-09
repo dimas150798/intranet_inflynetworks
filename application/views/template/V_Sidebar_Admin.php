b314e2d77c2f7947ed84cc0444ecbede3e508524
Dimas Bagus Wahyu Sudrajat <47965622+dimas150798@users.noreply.github.com>
2023-09-09 11:30:01 +0700

    Menambah fitur data laporan keluar

--- application/views/template/V_Sidebar_Admin.php
+++ application/views/template/V_Sidebar_Admin.php
@@ -180,7 +180,7 @@
                         </a>
                         <ul class="submenu">
                             <li><a href="introduction.html">Laporan Masuk</a></li>
-                            <li><a href="getting-started.html">Laporan Keluar</a></li>
+                            <li><a href="<?php echo base_url('admin/LaporanKeluar/C_Laporan_Keluar') ?>">Laporan Keluar</a></li>
                             <li><a href="getting-started.html">Peminjaman</a></li>
                         </ul>
                     </li>
