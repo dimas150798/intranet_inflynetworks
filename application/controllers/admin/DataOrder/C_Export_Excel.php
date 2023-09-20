156f13c61416a8aee2aac59c63db1802a613ba47
Dimas Bagus Wahyu Sudrajat <47965622+dimas150798@users.noreply.github.com>
2023-09-20 11:26:27 +0700

    Download Excel

--- application/controllers/admin/DataOrder/C_Export_Excel.php
+++ application/controllers/admin/DataOrder/C_Export_Excel.php
@@ -355,7 +355,7 @@ class C_Export_Excel extends CI_Controller
 
             /* Excel Header */
             $sheet->setCellValue('A1', 'PT. Urban Teknologi Nusantara');
-            $sheet->setCellValue('A2', 'LAPORAN PEMBELIAN PADA ' . ' Bulan ' . $this->session->userdata('bulan') . ' Tahun ' . $this->session->userdata('tahun'));
+            $sheet->setCellValue('A2', 'Laporan Transaksi Pengeluaran Pada ' . ' Bulan ' . $this->session->userdata('bulan') . ' Tahun ' . $this->session->userdata('tahun'));
 
             // Merubah ukuran font
             $spreadsheet->getActiveSheet()->getStyle('A1')->getFont()->setSize(17);
